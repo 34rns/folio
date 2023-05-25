4b461c1919811b55c90821ed749b39c3cf25a98b
RNS <95564509+34rns@users.noreply.github.com>
2023-05-25 16:29:34 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const imgM = document.getElementById("imgCng");
+const cngBtn = document.getElementById("cngBtn");
+let index = 0;
+let typingEffect = new Typed(".multi-text", {
+  strings: ["programmer", "gamer", "video editor", "coder"],
+  loop: true,
+  typeSpeed: 100,
+  backSpeed: 80,
+  backDelay: 1500,
+});
+
+cngBtn.addEventListener("click", (e) => {
+  if (index > 1) {
+    index = 0;
+  }
+  if (index === 0) {
+    imgM.setAttribute("src", "girlAnime.png");
+  } else if (index === 1) {
+    imgM.setAttribute("src", "mask.png");
+  }
+  index++;
+});
